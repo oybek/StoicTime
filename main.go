d67af85b4348501ecbc45d237576e53f37a1e57b
Oibek Khashimov <o.khashimov@eslgaming.com>
2025-04-27 06:40:50 +0600

    WIP

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"context"
+	_ "embed"
 	"fmt"
 	"log"
+	"regexp"
+	"slices"
+	"strings"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/sashabaranov/go-openai"
@@ -11,14 +15,38 @@ import (
 	"oybek.io/kerege/openaiw"
 )
 
-const text = `
-Кереге это умный консультант по кодексам и законам республики Кыргызстан
-`
+//go:embed assets/taxcode.txt
+var taxCode string
+
+func splitByArticle(text string) []string {
+	re := regexp.MustCompile(`(?m)^Статья \d+\.`)
+	matches := re.FindAllStringIndex(text, -1)
+
+	blocks := make([]string, len(matches))
+	for i, match := range matches {
+		start := match[0]
+		// If it's the last match, the block ends at the end of the string.
+		end := len(text)
+		if i+1 < len(matches) {
+			end = matches[i+1][0]
+		}
+
+		block := strings.TrimSpace(text[start:end])
+		blocks = append(blocks, block)
+	}
+
+	return blocks
+}
 
 func main() {
 	ctx := context.Background()
 
-	//
+	blocks := splitByArticle(taxCode)
+	slices.SortFunc(blocks, func(a, b string) int {
+		return len(b) - len(a)
+	})
+	log.Printf("Кол-во слов: %d\n%s", len(strings.Fields(blocks[1])), blocks[1])
+
 	theConfig, err := config.NewConfig()
 	if err != nil {
 		log.Fatalf("error create config: %v", err)
@@ -37,7 +65,8 @@ func main() {
 	}
 	defer pgConn.Close(ctx)
 
-	embedding, _ := openAIWrapper.GetEmbedding(ctx, text)
+	embedding, err := openAIWrapper.GetEmbedding(ctx, blocks[1])
 
-	fmt.Printf("%#v\n", embedding)
+	fmt.Printf("error: %#v\n", err)
+	fmt.Printf("embedding: %#v\n", embedding)
 }
